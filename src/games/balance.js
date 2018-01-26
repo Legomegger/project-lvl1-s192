c7adfef12cc5ccbb20192993b57646d1f47ecafb
Alikhan Askarov <legomegakst@gmail.com>
2018-01-26 12:48:42 +0300

    step7

--- src/games/balance.js
+++ src/games/balance.js
@@ -5,16 +5,24 @@ const rules = 'Balance the given number.';
 const game = () => {
   const randomNumberGenerator = () => Math.floor(Math.random() * (1000));
   const question = randomNumberGenerator();
-  const isBalanced = (n) => {
-    if (n <= 9) {
-      return true;
+  const balance = (n) => {
+    const workingNumber = String(n).split('').map(Number);
+    const high = Math.max.apply(null, workingNumber);
+    const low = Math.min.apply(null, workingNumber);
+    const isBalanced = (a, b) => {
+      if (a - b <= 1) {
+        return true;
+      }
+      return false;
+    };
+    if (isBalanced(high, low) === true) {
+      return workingNumber.sort((min, max) => min - max).join('');
     }
-    const strNum = String(n).split('');
-    return console.log(strNum);
+    workingNumber[workingNumber.indexOf(high)] -= 1;
+    workingNumber[workingNumber.indexOf(low)] += 1;
+    return balance(workingNumber.join(''));
   };
-  console.log(question);
-  console.log(isBalanced(question))
-  const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+  const correctAnswer = balance(question);
   return cons(question, String(correctAnswer));
 };
 
